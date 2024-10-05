061ba059d6d32fdab75000cf9cb4ba234e92cd8a
kristiFeed <160482366+kristiFeed@users.noreply.github.com>
2024-10-06 01:46:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            const double Pi = 3.14;
+
+            Console.WriteLine("Введіть r = ");
+            string S1 = Console.ReadLine();
+            int r = int.Parse(S1);
+
+            Console.WriteLine("Введіть h = ");
+            string S2 = Console.ReadLine();
+            int h = int.Parse(S2);
+
+            double S = 2 * Pi * r * h;
+            double V = Pi * r * r * h;
+
+            S = Math.Round(S, 1);
+            V = Math.Round(V, 1);
+
+            Console.WriteLine("Площа бічної поверхні: " + S);
+            Console.WriteLine("Об'єм: " + V);
+        }
+    }
+}
